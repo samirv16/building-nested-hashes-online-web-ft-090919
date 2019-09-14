3d780f799140a419c4bf078cb23349c07bd65095
edras vallecillo <samir.vallecillo@outlook.com>
2019-09-14 04:01:20 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -1,10 +1,11 @@
 def second_challenge
   epic_tragedy = {
-    :montague => {
-      #your key/value pairs here!
+    montague: {
+      patriarch: {},
+      matriarc
       }, 
-    :capulet => {
-      #your key/value pairs here!
+    capulet: {
+      
       }}
 
   
